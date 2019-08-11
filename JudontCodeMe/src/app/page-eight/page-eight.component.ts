c6b627f461d821bf0c8678d58ad4104441fcc2a4
RichardGomez0 <47157407+RichardGomez0@users.noreply.github.com>
2019-08-11 11:05:22 -0500

    Angular Portion of Final Project
    Includes HTML skeleton and navigation. Missing node_modules folder. (Make sure to use 'npm install' to get all the files on local PC)

---
+++ JudontCodeMe/src/app/page-eight/page-eight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-eight',
+  templateUrl: './page-eight.component.html',
+  styleUrls: ['./page-eight.component.css']
+})
+export class PageEightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
