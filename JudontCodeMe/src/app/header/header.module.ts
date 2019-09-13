56d4fb175e4083f250007bce6cd46022133cbe1a
inkeddrawnfish <Amber.Gallegos@my.scitexas.edu>
2019-09-13 05:17:17 -0400

    Update header.module.ts

--- JudontCodeMe/src/app/header/header.module.ts
+++ JudontCodeMe/src/app/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnimatedLogoModule } from '../animated-logo/animated-logo.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AnimatedLogoModule
   ]
 })
 export class HeaderModule { }
